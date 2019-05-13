cc8508dab6d23ebbd20e929f54bfe197887efdab
Siming Gao <gsm988aa@gmail.com>
2019-05-13 12:59:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/WebViewJavaScriptInterface.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+/**
+ * This class contains all of the functions used by JavaScript to call Java Functions
+ */
+public class WebViewJavaScriptInterface {
+
+    private MainActivity context;
+
+    /*
+     * Need a reference to the context in order to execute Java code.
+     */
+    public WebViewJavaScriptInterface(MainActivity context) {
+        this.context = context;
+    }
+
+    /**
+     * Use the native application's toast object to post a notification on the screen.
+     * @param message String
+     * @param lengthLong boolean
+     */
+    @JavascriptInterface
+    public void makeToast(String message, boolean lengthLong) {
+        Toast.makeText(context, message, (lengthLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT)).show();
+    }
+
+    /**
+     * Change activities within the application.
+     */
+    @JavascriptInterface
+    public void goToSecondActivity() {
+        context.goToSecondActivity();
+    }
+    @JavascriptInterface
+    public void goToSecond2Activity() {
+        context.goToSecond2Activity();
+    }
+
+}
